dfb2da1b4722d5fe918e7935771c1b9cf317c417
academy_learner <academy_learner@academy-learners-MacBook-Air.local>
2021-05-21 16:02:45 +0200

    class object

--- Class-Binding/src/app/test/test.component.ts
+++ Class-Binding/src/app/test/test.component.ts
@@ -8,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
 export class TestComponent implements OnInit {
   public name = 'Class_Binding';
   public successClass = 'text-success';
-  public hasError = 'true'
+  public hasError = 'false'
+  public isSpecial = true;
+  public messageClass = {
+    "text-success": !this.hasError,
+    "text-danger":this.hasError,
+    "text-special":this.isSpecial
+  }
 
   constructor() {}
 
