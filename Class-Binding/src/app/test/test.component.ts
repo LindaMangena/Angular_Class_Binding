eac7afb5520df3aafda68401c25551a369e75974
academy_learner <academy_learner@academy-learners-MacBook-Air.local>
2021-05-21 11:55:18 +0200

    error class

--- Class-Binding/src/app/test/test.component.ts
+++ Class-Binding/src/app/test/test.component.ts
@@ -3,14 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
-  styleUrls: ['./test.component.css']
+  styleUrls: ['./test.component.css'],
 })
 export class TestComponent implements OnInit {
-  public name = 'Class_Binding'
+  public name = 'Class_Binding';
+  public successClass = 'text-success';
+  public hasError = 'true'
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
